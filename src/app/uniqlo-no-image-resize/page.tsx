6ea34ebd9f0e4ed65406dd3b626b3299fbe38ea7
Vito-Ethan <evito@unr.edu>
2024-11-06 00:04:35 -0800

    Add uniqlo image sizing example

---
+++ src/app/uniqlo-no-image-resize/page.tsx
@@ -0,0 +1,19 @@
+// uniqlo has a weird feature when you resize their page vertically, the video playing in the background doesn't resize. part of the image
+// just gets cropped as you size up.
+// press f12 and slide the dev tools up or just resize the browser vertically.
+// you'll notice the image just gets cropped
+import Image from 'next/image';
+export default function Page() {
+  return (
+    <section className="relative flex flex-col h-screen overflow-hidden md:block justify-center">
+      <Image
+        src="/sean.jpg"
+        priority
+        alt="bob"
+        width={1920}
+        height={1080}
+        quality={100}
+      />
+    </section>
+  );
+}
