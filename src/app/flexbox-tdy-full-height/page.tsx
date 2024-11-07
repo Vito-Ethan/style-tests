5403669f8ee2b535807e96753313cf33aa1f6d51
Vito-Ethan <evito@unr.edu>
2024-11-06 17:27:14 -0800

    Add full height section

---
+++ src/app/flexbox-tdy-full-height/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen h-screen flex justify-center bg-blue-100">
+      <div className="border-black border w-1/2 h-full flex items-center">
+        <div className="h-10 bg-red-100 border-black border w-full">Text</div>
+        <div className="h-10 bg-red-100 border-black border w-full h-full">
+          Text 2
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@ import Button from './_components/button';
 
 export default function Home() {
   return (
-    <Button>
-      <Link href="/uniqlo-no-image-resize">Uniqlo No Resize</Link>
-    </Button>
+    <div className="min-h-screen flex flex-col justify-center gap-y-1">
+      <Button>
+        <Link href="/uniqlo-no-image-resize">Uniqlo No Resize</Link>
+      </Button>
+      <Button>
+        <Link href="/flexbox-tdy-full-height">Flexbox TDY Full Height</Link>
+      </Button>
+    </div>
   );
 }
